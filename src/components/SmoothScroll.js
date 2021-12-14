eed7799b42d32d4639af99e13192aba9708e8c8e
Husen Lokhandwala <coder.husen@gmail.com>
2021-12-14 23:37:30 +0530

    warning removed from smoothscroll

--- src/components/SmoothScroll.js
+++ src/components/SmoothScroll.js
@@ -3,17 +3,17 @@ import Scrollbar from 'smooth-scrollbar'
 import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll'
 
 const SmoothScroll = () => {
-    const options = {
-        damping: 0.07,
-        plugins: {
-            overscroll: {
-                effect: 'bounce',
-                damping: '0.15',
-                maxOverscroll: 150
+    useEffect(() => {
+        const options = {
+            damping: 0.07,
+            plugins: {
+                overscroll: {
+                    effect: 'bounce',
+                    damping: '0.15',
+                    maxOverscroll: 150
+                }
             }
         }
-    }
-    useEffect(() => {
         Scrollbar.use(OverscrollPlugin)
         Scrollbar.init(document.body, options)
     }, [])
